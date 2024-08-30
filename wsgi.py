674a2c3c309b97672c99b4c28d438382cf7478e3
partho8x <114854765+partho8x@users.noreply.github.com>
2024-08-30 16:44:48 +0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
-import requests
+from flask import Flask, render_template
 import os
-from bs4 import BeautifulSoup
-from tabulate import tabulate
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-            return render_template('index.html')
-    
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT",4000))
-    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))
     app.run(host='0.0.0.0', port=port)
